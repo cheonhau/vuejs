1026d2fa4d3d96a24e45a8c1166b4f5ac2a37035
mrjohuy <huynh@onetech.vn>
2019-09-09 22:22:35 +0700

    1

---
+++ standard-lv-vue/resources/js/components/ListUser.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="list-user">
+        this is List User
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+    
+</style>
