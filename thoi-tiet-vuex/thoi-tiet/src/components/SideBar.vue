304cbb8e5381c6547d759af82251685fefc4f0a8
huynh <huynh@onetech.vn>
2019-11-11 16:00:36 +0700

    dinh nghia action dom trong vue

--- thoi-tiet-vuex/thoi-tiet/src/components/SideBar.vue
+++ thoi-tiet-vuex/thoi-tiet/src/components/SideBar.vue
@@ -6,17 +6,26 @@
 import { TweenMax, Power4 } from 'gsap'
 export default {
     name: 'sidebar',
+    created() {
+        // lúc này có thể can thiệp đến data nhưng chưa can thiệp được tới dom
+        // thường được sử dụng để fetch data của api
+    },
     mounted () {
+        // lúc này có thể can thiệp tới data và tới dom
+        // thường gọi Dom bằng cách gọi this.$el : ví dụ this.$el.getElementsByTagName('tr')[0]
         TweenMax.set(this.$el, {
             x: this.$el.offsetWidth
-        })
+        });
     },
     computed: {
+        // nơi này thường để convert filter lại những data để xử lý
+        // những data nơi đây sẽ bị cache lại
         open () {
             return this.$store.state.ui.sidebarOpen
         }
     },
     watch: {
+        // nơi này sẽ luôn lắng nghe sự thay đổi data
         open: function (open) {
             const dX = open ? 0 : this.$el.offsetWidth
             TweenMax.to(this.$el, 0.6, {
@@ -24,7 +33,10 @@ export default {
                 ease: Power4.easeOut
             })
         }
-    }
+    },
+    methods: {
+        // nơi này sẽ viết các phương thức cần thiết
+    },
 }
 </script>
 
